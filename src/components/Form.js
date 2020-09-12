60e9bd8304598a43812d23d62dcdf2fae28b12c9
Cesar Alcancio <cesar.alcancio@payclip.com>
2020-09-12 13:09:55 -0500

    React Routers

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from '@material-ui/core/Button';
 
-export class Form extends Component {
-    render() {
-        return (
-            <div>
-                <Button variant="contained" color="primary">Paula Honda</Button>
-            </div>
-        )
-    }
+function Form() {
+    return (
+        <div>
+            <Button variant="contained" color="primary">Paula Honda</Button>
+        </div>
+    );
 }
 
 export default Form;
