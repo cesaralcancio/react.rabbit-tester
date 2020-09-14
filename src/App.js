7c176656794d8db054c3b50afd9ebc1b4bd73b78
Cesar Alcancio <cesar.alcancio@payclip.com>
2020-09-13 19:28:52 -0500

    Listing tasks

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Pokemon from './components/Pokemon';
+import ListTask from './components/ListTasks';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    marginTop: theme.spacing(2)
   },
   paper: {
-    marginTop: theme.spacing(2),
     marginRight: theme.spacing(2),
   },
 }));
@@ -26,29 +26,32 @@ function App() {
   const classes = useStyles();
 
   return (
-    <main>
-      <Container component="main" maxWidth="lg">
-      <CssBaseline />
-        <div className={classes.root}>
+    <Container className={classes.root} component="main" maxWidth={false}>
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
           <Paper className={classes.paper}>
             <MenuList>
               <MenuItem><Link to="/">Home</Link></MenuItem>
               <MenuItem><Link to="/call-rabbit-form">Rabbit</Link></MenuItem>
               <MenuItem><Link to="/form">Form</Link></MenuItem>
               <MenuItem><Link to="/pokemon">Pokémon</Link></MenuItem>
+              <MenuItem><Link to="/task/list">List Task</Link></MenuItem>
               <MenuItem><Link to="/about">About</Link></MenuItem>
             </MenuList>
           </Paper>
-        </div>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/call-rabbit-form" component={CallRabbitForm_v1}/>
-          <Route path="/form" component={Form}/>
-          <Route path="/pokemon" component={Pokemon} />
-          <Route path="/about" component={About}/>
-        </Switch>
-      </Container>
-    </main>
+        </Grid>
+        <Grid item xs={10}>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/call-rabbit-form" component={CallRabbitForm_v1}/>
+            <Route path="/form" component={Form}/>
+            <Route path="/pokemon" component={Pokemon} />
+            <Route path="/task/list" component={ListTask} />
+            <Route path="/about" component={About}/>
+          </Switch>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/components/ListTasks.js
@@ -0,0 +1,83 @@
+import React, { useEffect } from 'react';
+import API from 'fetch-api';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+function ListTask() {
+
+    const refreshState = "nunca";
+    const [tasks, setTasks] = React.useState([]);
+    const fetchTasks = () => {
+        const api = new API({
+            baseURI: 'http://localhost:3001/'
+        });
+
+        api.get('tasks', (err, res, body) => {
+            if (body) {
+                setTasks(body);
+            }
+        });
+    }
+
+    useEffect(() => {
+        fetchTasks();
+    }, [refreshState]);
+
+    const useStyles = makeStyles({
+        table: {
+          minWidth: 650,
+        },
+        table_head_row_cell: {
+            fontWeight: "bolder",
+        }
+    });
+
+    const classes = useStyles();
+
+    return (
+        <div>
+            <h1>List Task</h1>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell className={classes.table_head_row_cell} align="left">ID</TableCell>
+                        <TableCell className={classes.table_head_row_cell} align="left">Task</TableCell>
+                        <TableCell className={classes.table_head_row_cell} align="left">Status</TableCell>
+                        <TableCell className={classes.table_head_row_cell} align="left">Created</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {tasks.map((row) => (
+                        <TableRow key={row.name}>
+                            <TableCell align="left">{row._id}</TableCell>
+                            <TableCell align="left">{row.name}</TableCell>
+                            <TableCell align="left">{row.status}</TableCell>
+                            <TableCell align="left">{row.created_date}</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <br/>
+            <br/>
+            {tasks.map((task) => (
+                <div key={task._id}>
+                    <div key={task._id}>{task._id}</div>
+                    <div>{task.name}</div>
+                    <div>{task.status}</div>
+                    <div>{task.created_date}</div>
+                    <br/>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default ListTask;
